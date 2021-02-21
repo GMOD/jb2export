6dc44fe2813621cd3b8d1df74385b9721304a902
Colin <colin.diesh@gmail.com>
2021-02-21 13:59:49 -0700

    Update automated script

--- README.md
+++ README.md
@@ -4,29 +4,52 @@ Static exports of JBrowse 2 rendering
 
 ## Setup
 
-This example currently uses the export_wiggle_svg branch, and indeed the
-rendered output is in svg
-
-Yarn link has trouble so I manually perform builds of the @jbrowse/core and
-various plugins and manually copy the results over
+This example currently uses the export_wiggle_svg branch, and uses that branch
+to generate the svg outputs seen in these exports (just svg exports currently,
+no png)
 
 ```
-yarn build&&rm -rf ~/jb2export/node_modules/@jbrowse/core && cp  -R dist ~/jb2export/node_modules/@jbrowse/core
+## setup jb2
+git clone https://github.com/GMOD/jbrowse-components
+git checkout export_svg_wiggle
+yarn
+cd ..
 
-yarn build&&rm -rf ~/jb2export/node_modules/@jbrowse/plugin-linear-genome-view && cp  -R dist ~/jb2export/node_modules/@jbrowse/plugin-linear-genome-view
-
-yarn build&&rm -rf ~/jb2export/node_modules/@jbrowse/plugin-wiggle && cp  -R dist ~/jb2export/node_modules/@jbrowse/plugin-wiggle
-
-yarn build&&rm -rf ~/jb2export/node_modules/@jbrowse/plugin-svg && cp  -R dist ~/jb2export/node_modules/@jbrowse/plugin-svg
+## setup jb2export
+git clone git@github.com:cmdcolin/jb2export
+cd jb2export
+./setup_node_modules /path/to/jb2/installation
+yarn build
 ```
+
+The setup_node_modules performs builds of the plugins on the export_svg_wiggle
+branch and copies them over (`yarn link` doesn't work well so they are manually
+built and copied)
 
 ## Usage
 
-Customize the src/index.js script
+First build the system with `yarn build` and generate dist/index.js, then we
+can run the command with custom json
 
-Run `yarn test` which builds and runs dist/index > out.svg
+```
+# generate out.svg using specified assembly, tracks, and session files
+node dist/index.js --assembly assembly.json --tracks tracks.json --session session.json > out.svg
+```
 
-Then svg can be manually converted to png
+If --assembly, --tracks, or --session aren't specified it uses the files in the
+data directory
+
+If needed the svg can then be manually converted to png, for example using the
+convert command from ImageMagick
+
+    convert out.svg out.png
+
+The inkscape command line does a slightly better job in some cases too,
+particularly with the text
+
+    inkscape --export-type png --export-filename out.png -w 2048 out.svg
+
+The screenshot below uses inkscape
 
 ## Example
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "regenerator-runtime/runtime";
+import fs from "fs";
 
 import "abortcontroller-polyfill/dist/abortcontroller-polyfill-only";
 import {
@@ -8,159 +9,34 @@ import {
   ThemeProvider,
 } from "@jbrowse/react-linear-genome-view";
 import { renderToSvg } from "@jbrowse/plugin-linear-genome-view";
+const yargs = require("yargs");
+
+const argv = yargs
+  .command("jb2img", "Creates jbrowse 2 image snapshots")
+  .option("config", {
+    description: "Path to config file",
+    type: "string",
+  })
+  .option("session", {
+    description: "Path to session file",
+    type: "string",
+  })
+  .option("tracks", {
+    description: "Path to tracks portion of a session",
+    type: "string",
+  })
+  .help()
+  .alias("help", "h").argv;
 
 // const theme = createJBrowseTheme();
 
-const assembly = {
-  name: "GRCh38",
-  sequence: {
-    type: "ReferenceSequenceTrack",
-    trackId: "GRCh38-ReferenceSequenceTrack",
-    adapter: {
-      type: "BgzipFastaAdapter",
-      fastaLocation: {
-        uri: "https://jbrowse.org/genomes/GRCh38/fasta/hg38.prefix.fa.gz",
-      },
-      faiLocation: {
-        uri: "https://jbrowse.org/genomes/GRCh38/fasta/hg38.prefix.fa.gz.fai",
-      },
-      gziLocation: {
-        uri: "https://jbrowse.org/genomes/GRCh38/fasta/hg38.prefix.fa.gz.gzi",
-      },
-    },
-  },
-  aliases: ["hg38"],
-  refNameAliases: {
-    adapter: {
-      type: "RefNameAliasAdapter",
-      location: {
-        uri:
-          "https://s3.amazonaws.com/jbrowse.org/genomes/GRCh38/hg38_aliases.txt",
-      },
-    },
-  },
-};
-
-const tracks = [
-  {
-    type: "FeatureTrack",
-    trackId: "repeats_hg38",
-    name: "Repeats",
-    assemblyNames: ["hg38"],
-    category: ["Annotation"],
-    adapter: {
-      type: "BigBedAdapter",
-      bigBedLocation: {
-        uri: "https://jbrowse.org/genomes/GRCh38/repeats.bb",
-      },
-    },
-  },
-  {
-    type: "FeatureTrack",
-    trackId: "ncbi_refseq_109_hg38",
-    name: "NCBI RefSeq (GFF3Tabix)",
-    assemblyNames: ["GRCh38"],
-    category: ["Annotation"],
-    adapter: {
-      type: "Gff3TabixAdapter",
-      gffGzLocation: {
-        uri:
-          "https://s3.amazonaws.com/jbrowse.org/genomes/GRCh38/ncbi_refseq/GCA_000001405.15_GRCh38_full_analysis_set.refseq_annotation.sorted.gff.gz",
-      },
-      index: {
-        location: {
-          uri:
-            "https://s3.amazonaws.com/jbrowse.org/genomes/GRCh38/ncbi_refseq/GCA_000001405.15_GRCh38_full_analysis_set.refseq_annotation.sorted.gff.gz.tbi",
-        },
-      },
-    },
-  },
-  {
-    type: "QuantitativeTrack",
-    trackId: "hg38.100way.phyloP100way",
-    name: "hg38.100way.phyloP100way",
-    category: ["Conservation"],
-    assemblyNames: ["hg38"],
-    adapter: {
-      type: "BigWigAdapter",
-      bigWigLocation: {
-        uri:
-          "http://hgdownload.cse.ucsc.edu/goldenpath/hg38/phyloP100way/hg38.phyloP100way.bw",
-      },
-    },
-  },
-  {
-    type: "FeatureTrack",
-    trackId: "clinGenGeneDisease",
-    name: "ClinGen Gene-Disease mapping",
-    assemblyNames: ["hg38"],
-    category: ["ClinGen"],
-    adapter: {
-      type: "BigBedAdapter",
-      bigBedLocation: {
-        uri:
-          "https://hgdownload.soe.ucsc.edu/gbdb/hg38/bbi/clinGen/clinGenGeneDisease.bb",
-      },
-    },
-  },
-];
-
-const defaultSession = {
-  name: "My session",
-  view: {
-    id: "linearGenomeView",
-    type: "LinearGenomeView",
-    offsetPx: 752150,
-    bpPerPx: 25.084820116171613,
-    displayedRegions: [
-      { assemblyName: "hg38", refName: "1", start: 0, end: 248956422 },
-    ],
-    tracks: [
-      {
-        id: "MYkD7Sl3E",
-        type: "FeatureTrack",
-        configuration: "ncbi_refseq_109_hg38",
-        displays: [
-          {
-            id: "eKHbYEqbSZ",
-            type: "LinearBasicDisplay",
-            height: 218,
-            configuration: "ncbi_refseq_109_hg38-LinearBasicDisplay",
-          },
-        ],
-      },
-      {
-        id: "RmEVOSCDv",
-        type: "FeatureTrack",
-        configuration: "clinGenGeneDisease",
-        displays: [
-          {
-            id: "sXi9Yt_n7f",
-            type: "LinearBasicDisplay",
-            height: 100,
-            configuration: "clinGenGeneDisease-LinearBasicDisplay",
-          },
-        ],
-      },
-      {
-        id: "gTxauXvlF",
-        type: "QuantitativeTrack",
-        configuration: "hg38.100way.phyloP100way",
-        displays: [
-          {
-            id: "xYlBLNTFi3",
-            type: "LinearWiggleDisplay",
-            height: 100,
-            configuration: "hg38.100way.phyloP100way-LinearWiggleDisplay",
-            selectedRendering: "",
-            resolution: 5,
-            constraints: {},
-          },
-        ],
-      },
-    ],
-  },
-};
+const assembly = JSON.parse(
+  fs.readFileSync(argv.config || "data/assembly.json")
+);
+const tracks = JSON.parse(fs.readFileSync(argv.tracks || "data/tracks.json"));
+const defaultSession = JSON.parse(
+  fs.readFileSync(argv.session || "data/session.json")
+);
 
 (async () => {
   try {
